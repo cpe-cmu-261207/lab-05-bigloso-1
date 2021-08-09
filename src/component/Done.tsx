e98bd1d79d2002104677bb7be2be0bff5debfd73
bigloso <59277363+bigloso@users.noreply.github.com>
2021-08-09 20:50:03 +0700

    แล้ว

--- src/component/Done.tsx
+++ src/component/Done.tsx
@@ -7,7 +7,7 @@ type Donetype={
         <div
         className="flex justify-between h-8 items-center py-6 border-b"
       >
-        <span className="text-2xl line-through"> {name}</span>
+        <span className="text-2xl font-serif line-through"> {name}</span>
       </div>
     )
   }
