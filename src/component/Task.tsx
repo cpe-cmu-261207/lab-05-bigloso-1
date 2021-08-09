224e4ad2ef2adaec44f254863dc2ffc864afb09b
bigloso <59277363+bigloso@users.noreply.github.com>
2021-08-09 20:50:32 +0700

    แล้ว

--- src/component/Task.tsx
+++ src/component/Task.tsx
@@ -13,8 +13,8 @@ type TaskProps = {
     >
       <span className="text-2xl">{name}</span>
       <div className="flex space-x-1 items-center opacity-0 group-hover:opacity-100 ">
-        <button className="bg-green-400 w-24 text-2xl" onClick={ () => doneFn(id) } >Done</button>
-        <button className="bg-red-400 w-24 text-2xl" onClick={ () =>  deleteFn(id)} >Delete</button>
+        <button className="bg-green-400 font-serif w-24 text-2xl" onClick={ () => doneFn(id) } >Done</button>
+        <button className="bg-red-400 font-serif w-24 text-2xl" onClick={ () =>  deleteFn(id)} >Delete</button>
       </div>
     </div>
     )
